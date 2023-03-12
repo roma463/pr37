09483ba731f1691423025397259cf898debdc7e9
roma463 <461noname.error4o4@gmail.com>
2023-03-12 17:01:49 +0700

    я поменял

--- main.py
+++ main.py
@@ -14,8 +14,41 @@ Builder.load_file('test.kv')
 Window.size = (360, 800)
 
 
-questions = ["вопhujik","вопрос 2", "вопрос 3"]
-questionsImage = ["12.png","goblin.jpeg", "start.jpeg"]
+questions = ["вопрос1","вопрос 2", "вопрос 3",
+            "вопрос1","вопрос 2", "вопрос 3",
+            "вопрос1","вопрос 2", "вопрос 3",
+            "вопрос1","вопрос 2", "вопрос 3",
+            "вопрос1","вопрос 2", "вопрос 3",
+            "вопрос1","вопрос 2", "вопрос 3",
+            "вопрос1","вопрос 2"]
+questionsImage = ["12.png","goblin.jpeg", "start.jpeg",
+                "12.png","goblin.jpeg","start.jpeg",
+                "12.png","goblin.jpeg", "start.jpeg",
+                "12.png","goblin.jpeg", "start.jpeg",
+                "12.png","goblin.jpeg", "start.jpeg",
+                "12.png","goblin.jpeg", "start.jpeg"
+                "12.png","goblin.jpeg"]
+
+variants_otvet = [["11","12","13","14"],#1
+                ["21","22","23","24"],#2
+                ["31","32","33","34"],#3
+                ["41","42","43","44"],#4
+                ["","","",""],#5
+                ["","","",""],#6
+                ["","","",""],#7
+                ["","","",""],#8
+                ["","","",""],#9
+                ["","","",""],#10
+                ["","","",""],#11
+                ["","","",""],#12
+                ["","","",""],#13
+                ["","","",""],#14
+                ["","","",""],#15
+                ["","","",""],#16
+                ["","","",""],#17
+                ["","","",""],#18
+                ["","","",""],#19
+                ["","","",""],]#20
 
 summ = 0
 
@@ -32,6 +65,15 @@ class TestWidget(Widget):
             questionsImage.remove(questionsImage[i])
             questions.remove(questions[i])
             summ = summ + int(string)
+            
+            arrayOtvet = variants_otvet[i]
+            variants_otvet.remove(variants_otvet[i])
+            self.ids.button1.text = arrayOtvet[0]
+            self.ids.button2.text = arrayOtvet[1]
+            self.ids.button3.text = arrayOtvet[2]
+            self.ids.button4.text = arrayOtvet[3]
+
+
             print(summ)
         else:
             if(summ > 0 or summ < 3):
